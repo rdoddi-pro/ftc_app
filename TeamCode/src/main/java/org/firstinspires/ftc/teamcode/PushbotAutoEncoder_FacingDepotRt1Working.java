fd706646d93a9396c47047a42808c50295439811
siddhpatani <siddhrupal@gmail.com>
2018-12-23 16:49:14 -0800

    Route 1 facing depot (gold mineral in the middle): Working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoEncoder_FacingDepotRt1Working.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoEncoder_FacingDepotRt1Working.java
@@ -66,46 +66,47 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  */
 
 @Autonomous(name="Route1-FacingDepot", group="Pushbot")
-public class Route1FacingDepot extends LinearOpMode {
-    public DcMotor  leftDrive   = null;
-    public DcMotor  rightDrive  = null;
-    public DcMotor  leftDrive2  = null;
-    public DcMotor  rightDrive2 = null;
+public class PushbotAutoEncoder_FacingDepotRt1Working extends LinearOpMode {
+    private DcMotor  leftDrive   = null;
+    private DcMotor  rightDrive  = null;
+    //public DcMotor  leftDrive2  = null;
+    //public DcMotor  rightDrive2 = null;
 
     /* Declare OpMode members. */
     private ElapsedTime     runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 8.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+    static final private double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final private double     DRIVE_GEAR_REDUCTION    = 1.0;     // This is < 1.0 if geared UP
+    static final private double     WHEEL_DIAMETER_INCHES   = 8.0 ;     // For figuring circumference
+    static final private double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.3;
-    static final double     TURN_SPEED              = 0.2;
+    static final private double     DRIVE_SPEED             = 0.3;
+    static final private double     TURN_SPEED              = 0.5;
 
     @Override
     public void runOpMode() {
-        leftDrive  = hardwareMap.get(DcMotor.class, "back_left");
-        rightDrive = hardwareMap.get(DcMotor.class, "back_right");
-        leftDrive2 = hardwareMap.get(DcMotor.class, "front_left");
-        rightDrive2 = hardwareMap.get(DcMotor.class, "front_right");
+        leftDrive  = hardwareMap.get(DcMotor.class, "front_left");
+        rightDrive = hardwareMap.get(DcMotor.class, "front_right");
+        //leftDrive2 = hardwareMap.get(DcMotor.class, "back_left");
+        //rightDrive2 = hardwareMap.get(DcMotor.class, "back_right");
+
         leftDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        leftDrive2.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        rightDrive2.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        //leftDrive2.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        //rightDrive2.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        leftDrive2.setPower(0);
-        rightDrive2.setPower(0);
+        //leftDrive2.setPower(0);
+        //rightDrive2.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         /*
          * Initialize the drive system variables.
@@ -113,27 +114,26 @@ public class Route1FacingDepot extends LinearOpMode {
          */
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.addData("Status", "Resetting Encoders");
         telemetry.update();
 
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
 
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Path0",  "Starting at %7d :%7d",
                             leftDrive.getCurrentPosition(),
-                            rightDrive.getCurrentPosition(),
-                            leftDrive.getCurrentPosition(),
                             rightDrive.getCurrentPosition());
+
 
 
 
@@ -144,16 +144,18 @@ public class Route1FacingDepot extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  12,  12,  10.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(DRIVE_SPEED, 20, 20, 5.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -32, -32, 10.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  45,45,10.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED, 30, -30, 5.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,   10, 10, 6.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(TURN_SPEED, 18, -18, 5.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,   10, 10, 5.0);
+        encoderDrive(TURN_SPEED, 5, -5,5.0);
+        encoderDrive(DRIVE_SPEED*2, 60, 60, 15.0);
 
 
 
-        //robot.leftClaw.setPosition(1.0);            // S4: Stop and close the claw.
-        //robot.rightClaw.setPosition(0.0);
+
+
         telemetry.addData("Path", "Complete");
         telemetry.update();
     }
@@ -166,7 +168,7 @@ public class Route1FacingDepot extends LinearOpMode {
      *  2) Move runs out of time
      *  3) Driver stops the opmode running.
      */
-    public void encoderDrive(double speed,
+    private void encoderDrive(double speed,
                              double leftInches, double rightInches,
                              double timeoutS) {
         int newLeftTarget;
@@ -180,21 +182,21 @@ public class Route1FacingDepot extends LinearOpMode {
             newRightTarget = rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
             leftDrive.setTargetPosition(newLeftTarget);
             rightDrive.setTargetPosition(newRightTarget);
-            leftDrive2.setTargetPosition(newLeftTarget);
-            rightDrive2.setTargetPosition(newRightTarget);
+            //leftDrive2.setTargetPosition(newLeftTarget);
+            //rightDrive2.setTargetPosition(newRightTarget);
 
             // Turn On RUN_TO_POSITION
             leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            leftDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rightDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //leftDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //rightDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             // reset the timeout time and start motion.
             runtime.reset();
             leftDrive.setPower(Math.abs(speed));
             rightDrive.setPower(Math.abs(speed));
-            leftDrive2.setPower(Math.abs(speed));
-            rightDrive2.setPower(Math.abs(speed));
+            //leftDrive2.setPower(Math.abs(speed));
+            //rightDrive2.setPower(Math.abs(speed));
 
 
 
@@ -212,23 +214,23 @@ public class Route1FacingDepot extends LinearOpMode {
                 telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
                 telemetry.addData("Path2",  "Running at %7d :%7d",
                                             leftDrive.getCurrentPosition(),
-                                            rightDrive.getCurrentPosition(),
-                                            leftDrive2.getCurrentPosition(),
-                                            rightDrive2.getCurrentPosition());
+                                            rightDrive.getCurrentPosition());
+                                            //leftDrive2.getCurrentPosition(),
+                                            //rightDrive2.getCurrentPosition());
                 telemetry.update();
             }
 
             // Stop all motion;
             leftDrive.setPower(0);
             rightDrive.setPower(0);
-            leftDrive2.setPower(0);
-            rightDrive2.setPower(0);
+            //leftDrive2.setPower(0);
+            //rightDrive2.setPower(0);
 
             // Turn off RUN_TO_POSITION
             leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            //rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            //leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
 
